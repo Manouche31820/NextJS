bf0d8927983fd2ccb65bc7bfaccf62fc6c2d421f
Manouche31820 <72274915+Manouche31820@users.noreply.github.com>
2024-05-27 20:52:44 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,10 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to </strong>{' '}
+            <a href="https://www.linkedin.com/in/emmanuel-baraton-98a76338/" className="text-blue-500">
+              my Online Resume
             </a>
-            , brought to you by Vercel.
           </p>
           <div className={styles.shape} />
           <Link
